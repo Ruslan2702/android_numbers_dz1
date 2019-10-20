d23cce318a42b785861914dc3ec0af5b87dbfe6f
Ruslan Shahaev <rshahaev@yandex-team.ru>
2019-10-20 11:28:27 +0300

    Created app

---
+++ app/src/main/java/ruslan2702/android_numbers_dz1/MainActivity.java
@@ -0,0 +1,14 @@
+package ruslan2702.android_numbers_dz1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/ruslan2702/android_numbers_dz1/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package ruslan2702.android_numbers_dz1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
